67024e2d24d6bf1c070cbf1a2aeb1bd04c223576
Abdelrhman R,Muhammed <90706154+AbdelrhmanReda17@users.noreply.github.com>
2022-04-03 11:57:16 -0700

    Delete tempCodeRunnerFile.cpp

--- tempCodeRunnerFile.cpp
+++
@@ -1,7 +0,0 @@
-else if(choosing == "3")
-        // {
-        //     cout << "\n=> Merge Filter" << endl;
-        //     load_Two_Image();
-        //     do_merge();
-        //     break;
-        // }
