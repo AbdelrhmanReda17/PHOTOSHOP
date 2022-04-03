62f757c722c5b465458d9736967f647d3e281b3e
mahmoudazizorignal <mahmoudazizorignal@gmail.com>
2022-04-03 14:48:16 +0200

    Updated Main Program

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,7 @@
+else if(choosing == "3")
+        // {
+        //     cout << "\n=> Merge Filter" << endl;
+        //     load_Two_Image();
+        //     do_merge();
+        //     break;
+        // }
